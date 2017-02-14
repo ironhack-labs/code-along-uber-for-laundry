6d602bc2fd3b3b342ca44fd61d097289c2f0bee8
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-02-14 00:21:41 -0500

    Add models

---
+++ solution-code/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  isLaunderer: { type: Boolean, default: false },
+  fee: { type: Number, default: null }
+});
+
+userSchema.set('timestamps', true);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
