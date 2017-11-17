a541746db3989ac21bafe900e533f47fd7de9658
gemaderus <hi@gemaderus.com>
2017-11-17 11:55:57 +0100

    Sixth iteration finished.

--- starter-code/routes/laundry.js
+++ starter-code/routes/laundry.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const User = require("../models/user");
+const LaundryPickup = require('../models/laundry-pickup');
 
 router.get('/dashboard', (req, res, next) => {
   res.render('laundry/dashboard');
@@ -37,5 +38,40 @@ router.get('/launderers', (req, res, next) => {
     });
   });
 });
+
+router.get('/launderers/:id', (req, res, next) => {
+  const laundererId = req.params.id;
+
+  User.findById(laundererId, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.render('laundry/launderer-profile', {
+      theLaunderer: theUser
+    });
+  });
+});
+
+router.post('/laundry-pickups', (req, res, next) => {
+  const pickupInfo = {
+    pickupDate: req.body.pickupDate,
+    launderer: req.body.laundererId,
+    user: req.session.currentUser._id
+  };
+
+  const thePickup = new LaundryPickup(pickupInfo);
+
+  thePickup.save((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/dashboard');
+  });
+});
+
 
 module.exports = router;
