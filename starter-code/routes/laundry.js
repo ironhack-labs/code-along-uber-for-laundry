4e2643b102310620357756465932869dbfbc4e0e
Rocio Garcia <gooblyn@gmail.com>
2017-11-17 11:33:22 +0100

    Iteration six complete

--- starter-code/routes/laundry.js
+++ starter-code/routes/laundry.js
@@ -1,6 +1,7 @@
 // routes/laundry.js
 const express = require('express');
 const User = require('../models/user');
+const LaundryPickup = require('../models/laundry-pickup');
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -46,6 +47,40 @@ router.get('/launderers', (req, res, next) => {
     res.render('laundry/launderers', {
       launderers: launderersList
     });
+  });
+});
+
+router.get('/launderers/:id', (req, res, next) => {
+  const laundererId = req.params.id;
+
+  User.findById(laundererId, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.render('laundry/launderer-profile', {
+      theLaunderer: theUser
+    });
+  });
+});
+
+router.post('/laundry-pickups', (req, res, next) => {
+  const pickupInfo = {
+    pickupDate: req.body.pickupDate,
+    launderer: req.body.laundererId,
+    user: req.session.currentUser._id
+  };
+
+  const thePickup = new LaundryPickup(pickupInfo);
+
+  thePickup.save((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/dashboard');
   });
 });
 
