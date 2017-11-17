867f1ea7da4d6208da18ac08b79a168f97917ef4
Rocio Garcia <gooblyn@gmail.com>
2017-11-17 10:17:01 +0100

    Third iteration

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -100,4 +100,20 @@ router.post('/login', (req, res, next) => {
   });
 });
 
+router.get('/logout', (req, res, next) => {
+  if (!req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
