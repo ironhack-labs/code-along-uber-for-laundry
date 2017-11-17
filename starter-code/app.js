cfac5b45b72606f6242d64006ce79bd783e647c9
Rocio Garcia <gooblyn@gmail.com>
2017-11-17 10:10:57 +0100

    Second iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -28,6 +30,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -63,4 +63,41 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
+router.get('/login', (req, res, next) => {
+  res.render('auth/login', {
+    errorMessage: ''
+  });
+});
+
+router.post('/login', (req, res, next) => {
+  const emailInput = req.body.email;
+  const passwordInput = req.body.password;
+
+  if (emailInput === '' || passwordInput === '') {
+    res.render('auth/login', {
+      errorMessage: 'Enter both email and password to log in.'
+    });
+    return;
+  }
+
+  User.findOne({ email: emailInput }, (err, theUser) => {
+    if (err || theUser === null) {
+      res.render('auth/login', {
+        errorMessage: `There isn't an account with email ${emailInput}.`
+      });
+      return;
+    }
+
+    if (!bcrypt.compareSync(passwordInput, theUser.password)) {
+      res.render('auth/login', {
+        errorMessage: 'Invalid password.'
+      });
+      return;
+    }
+
+    req.session.currentUser = theUser;
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
