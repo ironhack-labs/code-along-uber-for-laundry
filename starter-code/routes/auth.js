ff80185f7d3d9dc48482c5b810fb2f63f054cd3d
gemaderus <hi@gemaderus.com>
2017-11-17 10:34:42 +0100

    Thirds iteration finished.

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -99,5 +99,21 @@ router.post('/login', (req, res, next) => {
   });
 });
 
+router.get('/logout', (req, res, next) => {
+  if (!req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/');
+  });
+});
+
 
 module.exports = router;
