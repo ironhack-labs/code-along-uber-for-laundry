3885569b00fa8601118f6bc5f4a6e12cce923b63
aemartos <aemartos@gmail.com>
2018-12-03 14:49:37 +0100

    Minor changes

--- starter_code/public/javascripts/script.js
+++ starter_code/public/javascripts/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  $('pickupDate').each((i,e) => {
+    console.log(this);
+  });
 
 }, false);
